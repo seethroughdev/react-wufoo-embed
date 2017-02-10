4ea787ffca1f41536b00eafa56458d4eddbbc781
Adam L <username@users.noreply.github.com>
2017-02-09 22:44:25 -0800

    Removed tests

--- tests.webpack.js
+++
@@ -1,5 +0,0 @@
-// ES5 shims for Function.prototype.bind, Object.prototype.keys, etc.
-require('core-js/es5');
-
-var context = require.context('./__tests__', true, /-spec\.js$/);
-context.keys().forEach(context);
